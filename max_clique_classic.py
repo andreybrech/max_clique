c1e198b93514ba9dac8224f8bf399e05ed4b11c1
Viktoria Maximova <maximova14@gmail.com>
2019-11-22 01:18:14 +0300

    Minor improvements

--- max_clique_classic.py
+++ max_clique_classic.py
@@ -3,65 +3,39 @@ import copy
 
 class Graph(object):
     def __init__(self):
-        """
-        V- множество вершин
-        D - множество вершин, с возможностью их сортировки по степени
-        colors - нужен только для покраски по вершинам
-        """
         self.V = {}
-        self.D = []
-        self._D_sorted = False
-        self.used_colors = set()
-        self.coloring = dict()
 
     def add_edge(self, v1, v2):
         """
         add pair of vertexes to V
-        add each other to neigbours
+        add each other to neighbours
         """
         self._add_vertex_to_neighbours(v1, v2)
         self._add_vertex_to_neighbours(v2, v1)
         return
 
     def _add_vertex_to_neighbours(self, v1, v2):
-        # add vertex v2 to neigbours of vertex v1
+        # add vertex v2 to neighbours of vertex v1
         if v1 not in self.V:
             self.add_vertex(v1)
         if v2 in self.V[v1]['neighbours']:
             print('vertex already in neighbours')
             return
         self.V[v1]['neighbours'].add(v2)
-        # self.V[v1]['degree'] += 1
-        self._D_sorted = False
         return
 
     def add_vertex(self, v1):
-        # add one vertex to V
-        if v1 in self.V:  # если вершина есть в множестве, то все ок. Ее не надо добавлять
-            # МБ надо обновить ее степень?
-            #             v1.update_degree = len(self.neighbours(v1))
-            #             self.V[v1]['degree'] = self.degree(v1)
-            #             D[self.V[v1]['D_index'] ] = self.V[v1]['degree']
+        if v1 in self.V:
             print('vertex already in graph.Vertexes')
-
             return
         else:
             self.V[v1] = {}
             self.V[v1]['name'] = v1
             self.V[v1]['neighbours'] = set()
-            # self.V[v1]['degree'] = 0
-            #             self.V[v1]['degree_position'] = 0
-            self.D.append(self.V[v1])
-            #             self.V[v1]['D_index'] = len(self.D) - 1
-            self._D_sorted = False
-            #             self.V[v1]['color'] = None
-            #             self.V[v1]['neigbour_colors'] = set()
-
             return
 
 
 def read_dimacs_graph(file_path):
-    # Parse .col file and return graph object
     g = Graph()
     with open(file_path, 'r') as file:
         for line in file:
@@ -91,12 +65,11 @@ def build_problem(g=Graph()):
     for i in g.V:
         for j in x_range:
             if (j+1 is not i) and (j+1 not in g.V[i]['neighbours']):
-                mdl.add_constraint(x[i-1]+x[j] <= 1)
+                mdl.add_constraint(x[i-1] + x[j] <= 1)
 
     for i in x_range:
         mdl.add_constraint(x[i] <= 1)
-    for i in x_range:
-        mdl.add_constraint(x[i]>=0)
+        mdl.add_constraint(x[i] >= 0)
 
     mdl.maximize(mdl.sum(x[i] for i in x_range))
     return mdl
@@ -107,10 +80,10 @@ eps = 1e-5
 
 def log_solution(res):
     print("Objective", res.get_objective_value())
-    print("solution vars:", end=' ')
-    for i, j in res.iter_var_values():
-        print(i, j, sep=', ', end='; ')
-    print("")
+    # print("solution vars:", end=' ')
+    # for i, j in res.iter_var_values():
+    #     print(i, j, sep=', ', end='; ')
+    # print("")
 
 
 def is_int_solution(sol):
@@ -119,28 +92,35 @@ def is_int_solution(sol):
     for vars in sol.iter_var_values():
         if vars[1] % 1 > eps and abs(vars[1] % 1 - 1) > eps:
             return False
+    print("int solution")
+    for i, j in sol.iter_var_values():
+        print(i, j, sep=', ', end='; ')
+    print("")
     return True
 
 
 class Solver:
     def __init__(self, objective, vars):
-        self.lower_bound = objective + 100000
+        self.upper_bound = objective
         self.vars = vars
+        self.current_best = 0
 
     def search(self, model):
         m = copy.deepcopy(model)
         self.branch_and_bound_search(m)
-        return self.lower_bound, self.vars
+        return self.upper_bound, self.vars
 
     def branch_and_bound_search(self, model):
         s = model.solve(log_output=False)
-        print("number of constraints:", model.number_of_constraints)
+        # print("number of constraints:", model.number_of_constraints)
         if s is None:
             print("No solution")
             return
         log_solution(s)
         if not is_int_solution(s):
-            if s.get_objective_value() > self.lower_bound:
+            if s.get_objective_value() > self.upper_bound:
+                return
+            if s.get_objective_value() < self.current_best:
                 return
             # branching
             var_dict = tuple()
@@ -167,22 +147,22 @@ class Solver:
             self.branch_and_bound_search(model)
             model.remove_constraint(con2)
         else:
-            if s.get_objective_value() < self.lower_bound:
-                self.lower_bound = s.get_objective_value()
+            if s.get_objective_value() > self.current_best:
+                self.current_best = s.get_objective_value()
                 self.vars = s.iter_var_values()
-                print("* New best lower bound:", self.lower_bound)
+                print("* New best:", self.current_best)
             else:
-                print(s.get_objective_value(), ">= lower_bound(", self.lower_bound, ")")
+                print(s.get_objective_value(), "<= current_best(", self.current_best, ")")
+                if s.get_objective_value() < self.upper_bound:
+                    self.upper_bound = s.get_objective_value()
 
 
 if __name__ == '__main__':
-    path_test = 'test.txt'
+    # path_test = 'C125.9.clq.txt'
+    path_test = 'test2.txt'
     g = read_dimacs_graph(path_test)
     model = build_problem(g)
-    # sol = model.solve(log_output=True)
-    # if sol is not None:
-    #     model.print_solution()
-    # pass
+
     sol = model.solve(log_output=True)
     if sol is not None:
         model.print_solution()
@@ -195,14 +175,7 @@ if __name__ == '__main__':
         print("Solution vars:")
         for c in vars:
             print(c[0], c[1])
-
         print("\nBranch-and-Bounded from:")
         model.print_solution()
-
-        # print("\nCPLEX solution for this problem (int vars)")
-        # model = build_problem(int_vars=True)
-        # model.solve(log_output=False)
-        # model.print_solution()
-
     else:
         print("Model is infeasible")
